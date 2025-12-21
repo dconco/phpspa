044053b114f36c5c4079dbde7001b820a8b0b3d5
onipededavid208@gmail.com <onipededavid208@gmail.com>
2025-12-21 03:33:19 +0100

    feat: Enhance HTTP client functionality and improve layout responsiveness

--- vite-template/app/layout/layout.php
+++ vite-template/app/layout/layout.php
@@ -1,10 +1,15 @@
 <?php
 
+require_once 'app/components/svgs/MenuIcon.php';
+
 $layout = function() use (&$app) {
+
    $html = file_get_contents('index.html');
 
    // --- Check if Vite dev server is running ---
-   $viteRunning = @file_get_contents('http://localhost:5173') !== false;
+   // $viteRunning = useFetch('http://localhost:5173')->timeout(1)->get()->text();
+
+   $viteRunning = strpos($html, '@vite/client') !== false;
 
    if (!$viteRunning) {
       // --- Production: Load assets from manifest ---
@@ -21,18 +26,9 @@ $layout = function() use (&$app) {
          }
       }
 
-      // --- Add CSS link tags for all imported stylesheets to the Application ---
+      // --- Add Production Javascript tags for the main Application script ---
 
       $app?->script('/assets/' . $mainEntry['file'], null, 'module');
-
-      // --- Remove all dev script (eg., main.ts, @vite/client) in production ---
-
-      $html = str_replace(
-         [
-            '<script type="module" src="http://localhost:5173/@vite/client"></script>',
-            '<script type="module" src="http://localhost:5173/src/main.ts"></script>'
-         ], '', $html, $count
-      );
    }
 
    return $html;
